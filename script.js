8a9d1a43254bad00e4934ef57e2ec513a8970fa9
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-22 17:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const gridContainer = document.getElementById("grid-container");
-  const gridItems = document.querySelectorAll(".grid-item");
-  const blockIdInput = document.getElementById("block_id");
-  const colorInput = document.getElementById("colour_id");
-  const changeButton = document.getElementById("change_button");
-  const resetButton = document.getElementById("Reset");
+	const gridContainer = document.getElementById("grid-container");
+	const gridItems = document.querySelectorAll(".grid-item");
+	const blockIdInput = document.getElementById("block_id");
+	const colorInput = document.getElementById("colour_id");
+	const changeButton = document.getElementById("change_button");
+	const resetButton = document.getElementById("Reset");
 
-  // Initialize grid background colors
-  gridItems.forEach((item) => {
-    item.style.backgroundColor = "transparent";
-  });
+	// Initialize grid background colors
+	gridItems.forEach((item) => {
+		item.style.backgroundColor = "transparent";
+	});
 
-  // Handle color change when the "Change Color" button is clicked
-  changeButton.addEventListener("click", function () {
-    const blockId = blockIdInput.value;
-    const selectedColor = colorInput.value;
+	// Handle color change when the "Change Color" button is clicked
+	changeButton.addEventListener("click", function () {
+		const blockId = blockIdInput.value;
+		const selectedColor = colorInput.value;
 
-    const block = document.getElementById(blockId);
+		const block = document.getElementById(blockId);
 
-    if (block) {
-      // Set the background color of the selected block
-      block.style.backgroundColor = selectedColor;
-    } else {
-      alert("Block ID not found!");
-    }
-  });
+		if (block) {
+			// Set the background color of the selected block
+			block.style.backgroundColor = selectedColor;
+		} else {
+			alert("Block ID not found!");
+		}
+	});
 
-  // Reset all grid items to transparent background
-  resetButton.addEventListener("click", function () {
-    gridItems.forEach((item) => {
-      item.style.backgroundColor = "transparent";
-    });
-  });
+	// Reset all grid items to transparent background
+	resetButton.addEventListener("click", function () {
+		gridItems.forEach((item) => {
+			item.style.backgroundColor = "transparent";
+		});
+	});
 });
